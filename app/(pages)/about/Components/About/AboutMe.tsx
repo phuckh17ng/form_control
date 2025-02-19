6a807b4480e085130eaf77221ca709f9418cca0f
khanglmp <lemauphuckhang171001@gmail.com>
2025-02-19 22:46:37 +0700

    update

--- app/(pages)/about/Components/About/AboutMe.tsx
+++ app/(pages)/about/Components/About/AboutMe.tsx
@@ -1,14 +1,61 @@
-import TextHyperLink from "@/app/components/Text";
-import React from "react";
+import { TextClip, TextHyperLink } from "@/app/components/Text";
+import { cubicBezier, easeInOut, motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const AboutMe = () => {
+  const scrollRef = useRef(null);
+  const isInView = useInView(scrollRef, { once: true });
+  const linkVariants = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.15, delayChildren: 0.5 },
+    },
+  };
+
+  const textVariants = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.15, delayChildren: 1 },
+    },
+  };
+
   return (
     <section className="py-24 w-full h-full">
-      <div className="grid grid-cols-12 pt-48">
-        <div className="col-span-11 col-start-0 text-end text-4xl tracking-wider mb-24 border-r-4 pr-4 border-[#eb5930]">
-          About me
-        </div>
-        <div className="col-span-3 col-start-3">
+      <div className="grid grid-cols-12 pt-48" ref={scrollRef}>
+        <motion.div className="relative col-span-11 col-start-0 mb-24 pr-4 overflow-hidden">
+          <motion.div
+            className="absolute top-0 right-0 border-r-4 border-[#eb5930] z-10"
+            initial={{ opacity: 0, height: 0, borderStyle: "none" }}
+            whileInView={{
+              opacity: 1,
+              height: "100%",
+              borderStyle: "solid",
+            }}
+            transition={{
+              duration: 0.5,
+              delay: 1.5,
+              ease: cubicBezier(1, 0.02, 0.31, 1),
+            }}
+            viewport={{ root: scrollRef, amount: 0.8, once: true }}
+          ></motion.div>
+          <motion.p
+            className="text-end text-4xl tracking-wider font-semibold text-primary"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 2, ease: easeInOut }}
+            viewport={{ root: scrollRef, amount: 0.8, once: true }}
+          >
+            About me
+          </motion.p>
+        </motion.div>
+        <motion.div
+          className="col-span-3 col-start-3"
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+          variants={linkVariants}
+        >
           <TextHyperLink
             onClick={() => {
               document.location = "mailto:khang17.dev@gmail.com";
@@ -21,34 +68,31 @@ const AboutMe = () => {
           <TextHyperLink>Facebook</TextHyperLink>
           <TextHyperLink>Instagram</TextHyperLink>
           <TextHyperLink>Resume</TextHyperLink>
-        </div>
-        <div className="col-end-12 col-span-6 justify-self-end text-2xl tracking-widest leading-normal max-lg:text-xl text-primary/70">
-          My work is mainly focused on third-dimension modeling, texturing and
-          rendering. I like exploring the creatures with a touch of dark
-          surrealism for characters and production.
-          <br />
-          <br /> Now I&apos;m an interactive media design student in Istanbul ⏤
-          currently freelancing and seeking internship opportunities. My work is
-          mainly focused on third-dimension modeling, texturing and rendering. I
-          like exploring the creatures with a touch of dark surrealism for
-          characters and production.
-          <br />
-          <br /> Now I&apos;m an interactive media design student in Istanbul ⏤
-          currently freelancing and seeking internship opportunities. My work is
-          mainly focused on third-dimension modeling, texturing and rendering. I
-          like exploring the creatures with a touch of dark surrealism for
-          characters and production.
-          <br />
-        </div>
-        {/* <Image
-      src="/images/vagabond.png"
-      width={500}
-      height={500}
-      alt="vagabond"
-      className={clsx(
-        "aspect-auto flex col-end-12 col-span-6 justify-self-end"
-      )}
-    /> */}
+        </motion.div>
+        <motion.div
+          className="col-end-12 col-span-6 justify-self-end"
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+          variants={textVariants}
+        >
+          <TextClip className="text-2xl tracking-widest leading-normal max-lg:text-xl text-primary/70">
+            My work is mainly focused on third-dimension modeling,
+          </TextClip>
+          <TextClip className="text-2xl tracking-widest leading-normal max-lg:text-xl text-primary/70">
+            texturing and rendering. I like exploring the creatures
+          </TextClip>
+          <TextClip className="text-2xl tracking-widest leading-normal max-lg:text-xl text-primary/70">
+            with a touch of dark surrealism for characters and production.
+          </TextClip>
+          <br /> <br />
+          <TextClip className="text-2xl tracking-widest leading-normal max-lg:text-xl text-primary/70">
+            Now I&apos;m an interactive media design student in Istanbul ⏤
+            currently freelancing and seeking internship opportunities. My work
+            is mainly focused on third-dimension modeling, texturing and
+            rendering. I like exploring the creatures with a touch of dark
+            surrealism for characters and production.
+          </TextClip>
+        </motion.div>
       </div>
     </section>
   );
--- app/components/Text/index.tsx
+++ app/components/Text/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import clsx from "clsx";
+import { motion } from "framer-motion";
 import { ReactNode } from "react";
 import styles from "./styles.module.css";
 
@@ -8,17 +9,43 @@ type Props = {
   children: ReactNode;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-export default function TextHyperLink(props: Props) {
-  const { children, ...rest } = props;
+export function TextHyperLink(props: Props) {
+  const { children } = props;
+  const itemVariants = {
+    hidden: { opacity: 0, y: 24 },
+    visible: { opacity: 1, y: 0 },
+  };
   return (
-    <div
-      {...rest}
-      className={clsx(
-        "py-2 my-4 text-2xl tracking-wider leading-normal max-lg:text-xl w-fit relative uppercase font-semibold",
-        styles.textContainer
-      )}
-    >
-      {children}
+    <div className={clsx("py-2 my-4 w-fit relative", styles.textContainer)}>
+      <div className="overflow-hidden w-fit h-fit">
+        <motion.p
+          className="text-2xl tracking-wider leading-normal max-lg:text-xl uppercase font-semibold"
+          transition={{ duration: 0.5 }}
+          variants={itemVariants}
+        >
+          {children}
+        </motion.p>
+      </div>
+    </div>
+  );
+}
+
+export function TextClip(props: Props) {
+  const { children, className } = props;
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  return (
+    <div className="overflow-hidden w-fit h-fit inline-block">
+      <motion.p
+        className={className}
+        transition={{ duration: 0.5 }}
+        variants={itemVariants}
+      >
+        {children}
+      </motion.p>
     </div>
   );
 }
